058d61a6eb4e8158f6670b32d45764ce25e7d0d8
Said Wattar <wattar.said@gmail.com>
2019-11-21 19:19:53 +0100

    first draft

---
+++ Server.py
@@ -0,0 +1,15 @@
+import socket
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+    msg = "välkommen!"
+    msg = (f'{len(msg):<20}'+msg)
+    clientsocket.send(bytes("velcome", "utf-8"))
+    
